85a0df235bfab49dfe60d39d77956b0b6611c411
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-01-11 20:50:05 +0100

    Automatically show documentation and ghost text

--- config/lua/plugins/completions.lua
+++ config/lua/plugins/completions.lua
@@ -11,6 +11,13 @@ require("blink.cmp").setup({
     nerd_font_variant = 'mono'
   },
   signature = { enabled = true, },
+  completion = {
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+    },
+    ghost_text = { enabled = true, },
+  },
   sources = {
     default = { 'lsp', 'path', 'snippets', 'buffer' },
     providers = {
